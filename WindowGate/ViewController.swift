ecb9dde49f67ba9a42996633faf5c7eae2f75ccf
Nobuhiro Ito <iseebi@iseteki.net>
2022-03-06 13:19:15 +0900

    Initial Commit

---
+++ WindowGate/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  WindowGate
+//
+//  Created by Nobuhiro Ito on 3/6/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
